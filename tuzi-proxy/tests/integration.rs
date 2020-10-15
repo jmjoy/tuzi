70565ac4a1fa46be34dad010e5a83410accd586d
jmjoy <918734043@qq.com>
2020-10-15 19:10:44 +0800

    Refactor integration tests.

--- tuzi-proxy/tests/integration.rs
+++ tuzi-proxy/tests/integration.rs
@@ -1,6 +1,6 @@
 mod setup;
 
-use crate::setup::{setup, Context};
+use crate::setup::{setup, Context, http1};
 use http::{Method, Request};
 use hyper::body;
 use nom::lib::std::alloc::handle_alloc_error;
@@ -10,37 +10,8 @@ use tokio::time::delay_for;
 #[tokio::test]
 async fn test_all() {
     let (context, mut handles) = setup().await;
-    handles.add_test(test_http1_index(context.clone())).await;
-    handles.add_test(test_http1_echo(context.clone())).await;
+    handles.add_test(http1::test_index(context.clone())).await;
+    handles.add_test(http1::test_echo(context.clone())).await;
     handles.wait().await;
 }
 
-async fn test_http1_index(context: Context) {
-    let client = context.build_client();
-    let uri = context.build_http_uri("/").await;
-    let response = client.get(uri).await.unwrap();
-    let status = response.status();
-    let content = body::to_bytes(response).await.unwrap();
-    let content = String::from_utf8(content.to_vec()).unwrap();
-
-    assert_eq!(status, 200);
-    assert_eq!(content, "Hello, World!");
-}
-
-async fn test_http1_echo(context: Context) {
-    let client = context.build_client();
-    let uri = context.build_http_uri("/echo").await;
-    let request = Request::builder()
-        .method(Method::POST)
-        .uri(uri)
-        .body("TEST ECHO".into())
-        .unwrap();
-    let response = client.request(request).await.unwrap();
-
-    let status = response.status();
-    let content = body::to_bytes(response).await.unwrap();
-    let content = String::from_utf8(content.to_vec()).unwrap();
-
-    assert_eq!(status, 200);
-    assert_eq!(content, "TEST ECHO");
-}
--- tuzi-proxy/tests/setup/http1.rs
+++ tuzi-proxy/tests/setup/http1.rs
@@ -6,6 +6,7 @@ use hyper::{
 use std::{convert::Infallible, future::Future, net::TcpListener as StdTcpListener};
 use tokio::sync::oneshot;
 use tracing::{debug, instrument};
+use crate::setup::Context;
 
 #[instrument(name = "test-server-http1:handle", skip(req))]
 async fn handle(req: Request<Body>) -> Result<Response<Body>, Infallible> {
@@ -29,4 +30,34 @@ pub async fn server(listener: StdTcpListener, signal: impl Future<Output = ()>) 
     let server = Server::from_tcp(listener).unwrap().serve(make_svc);
 
     server.with_graceful_shutdown(signal).await.unwrap();
+}
+
+pub async fn test_index(context: Context) {
+    let client = context.build_client();
+    let uri = context.build_http_uri("/").await;
+    let response = client.get(uri).await.unwrap();
+    let status = response.status();
+    let content = hyper::body::to_bytes(response).await.unwrap();
+    let content = String::from_utf8(content.to_vec()).unwrap();
+
+    assert_eq!(status, 200);
+    assert_eq!(content, "Hello, World!");
+}
+
+pub async fn test_echo(context: Context) {
+    let client = context.build_client();
+    let uri = context.build_http_uri("/echo").await;
+    let request = Request::builder()
+        .method(Method::POST)
+        .uri(uri)
+        .body("TEST ECHO".into())
+        .unwrap();
+    let response = client.request(request).await.unwrap();
+
+    let status = response.status();
+    let content = hyper::body::to_bytes(response).await.unwrap();
+    let content = String::from_utf8(content.to_vec()).unwrap();
+
+    assert_eq!(status, 200);
+    assert_eq!(content, "TEST ECHO");
 }
